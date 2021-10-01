78621ddcc526246713a7721afca494c15434f6fa
Yi Liu <elenaliu0415@gmail.com>
2021-09-30 21:09:02 -0700

    Project completed

--- js/script.js
+++ js/script.js
@@ -1,41 +1,34 @@
-// Given a time block element
-
-// let currentHour = get the current hour
-// let blockHour = get time block hour
-
-// IF currentHour < blockHour
-//    add "future" class to block element
-// ELSE IF currentHour > blockHour
-//    add "past" class to block element
-// ELSE
-//    add "present" class to block element
-
-// currentHour = 9
-// blockHour = 9
-
-// result adds "past" class to time block to make gray
-
-// moment js to show today's day and current time
-
-var todos = [];
 var saveButton = $(".saveBtn");
-
 var today = $("#currentDay").text(moment().format("LLLL"));
 
 saveButton.on("click", function() {
-    // console.log(this);
-    var hour = $(this).parent().attr("id");
-    var input = $(this).siblings(".description").val();
+
+    var button = $(this);
+    var hour = button.parent().attr("id");
+    var input = button.siblings(".description").val();
 
     localStorage.setItem(hour, input);
 })
 
-// $("#container").each(function () {
-//     var i = $(this).children("id").val(localStorage.getItem(i));
-//     var j = $(this).children("description").val(localStorage.getItem(j));
-//     console.log(i, j);
-// });
+var currentHour = moment().format("HH");
+       
+// Loop through each time slot
+$(".time-block").each(function() {
+    var hour = $(this).attr("id");
+    var timeSlotHr = hour.replace("hour-", "");
+    var saveData = localStorage.getItem(hour);
 
+    if(parseInt(currentHour) > parseInt(timeSlotHr)) {
+        $(this).addClass("past");
+    } else if(parseInt(currentHour) == parseInt(timeSlotHr)) {
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("future");
+    }
+    if(saveData != null) {
+        $(this).find(".description").val(saveData);
+    }
+});
 
 
 
